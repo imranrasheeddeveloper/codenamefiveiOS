201cc434e3cfe1af1eebe5da6c641f4a552ffa6e
muhammadimran <imranrasheed.developer@outlook.com>
2020-06-11 02:03:59 +0500

    Finalize

--- Controllers/SettingVCTableViewController.swift
+++ Controllers/SettingVCTableViewController.swift
@@ -10,100 +10,124 @@ import UIKit
 
 class SettingVCTableViewController: UITableViewController {
     
+    @IBOutlet weak var personImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.tableFooterView = UIView()
+        personImage.layer.cornerRadius = personImage.frame.size.width / 2
+        personImage.layer.shadowColor = UIColor(ciColor: .black).cgColor
+        personImage.layer.shadowRadius = 1
         
     }
-    // MARK: - Table view Delegate
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let currentSection = indexPath.section
-        if currentSection == 0{
-            if indexPath.row == 0 {
-                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyBoard.instantiateViewController(withIdentifier: "ProfileVC")
-                navigationController?.pushViewController(newViewController, animated: true)
-            }
-        }
-        if currentSection == 1{
-            if indexPath.row == 0 {
-                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyBoard.instantiateViewController(withIdentifier: "HistoryVC")
-                navigationController?.pushViewController(newViewController, animated: true)
-            }
-        }
-        
-        if currentSection == 2{
-            if indexPath.row == 2 {
-                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyBoard.instantiateViewController(withIdentifier: "EarningsVC")
-                navigationController?.pushViewController(newViewController, animated: true)
-            }
-        }
-        
-        if currentSection == 3 {
-            if indexPath.row == 0 {
-                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyBoard.instantiateViewController(withIdentifier: "HelpCenterVC")
-                navigationController?.pushViewController(newViewController, animated: true)
-            }
-        }
-        if currentSection == 4{
-            if indexPath.row == 0 {
-                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyBoard.instantiateViewController(withIdentifier: "MapSettingVC")
-                navigationController?.pushViewController(newViewController, animated: true)
-            }
-            if indexPath.row == 1 {
-                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyBoard.instantiateViewController(withIdentifier: "OpeninMapsVC")
-                navigationController?.pushViewController(newViewController, animated: true)
-            }
-        }
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return 100
-        }
-        else{
-            return 45
-        }
-    }
-    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        
-        let blurEffect = UIBlurEffect(style: .light)
-        let blurEffectView = UIVisualEffectView(effect: blurEffect)
-        
-        let headerView: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
-        headerView.textLabel!.textColor = UIColor.darkGray
-        headerView.textLabel!.font = UIFont(name: "Roboto-Bold", size: 15)
-        //headerView.tintColor = .groupTableViewBackground
-        headerView.backgroundView = blurEffectView
-        headerView.backgroundColor = UIColor(hex: "#F5F7F6")
-        
-        // For Header Text Color
-        let header = view as! UITableViewHeaderFooterView
-        header.textLabel?.textColor = .black
-        
-    }
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-         super.traitCollectionDidChange(previousTraitCollection)
 
-         if #available(iOS 13.0, *) {
-             if self.traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
-                 if traitCollection.userInterfaceStyle == .dark {
-                     print("dark mode")
-                 }
-                 else {
-                     //Light
-                 }
+}
+extension SettingVCTableViewController{
+    
+     // MARK: - Table view Delegate
+     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         
+         let currentSection = indexPath.section
+         if currentSection == 0{
+             if indexPath.row == 0 {
+                 let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                 let newViewController = storyBoard.instantiateViewController(withIdentifier: "ProfileVC")
+                 navigationController?.pushViewController(newViewController, animated: true)
              }
-         } else {
-             // Fallback on earlier versions
+         }
+         if currentSection == 1{
+             if indexPath.row == 0 {
+                 let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                 let newViewController = storyBoard.instantiateViewController(withIdentifier: "HistoryVC")
+                 navigationController?.pushViewController(newViewController, animated: true)
+             }
+         }
+         
+         if currentSection == 2{
+             if indexPath.row == 2 {
+                 let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                 let newViewController = storyBoard.instantiateViewController(withIdentifier: "EarningsVC")
+                 navigationController?.pushViewController(newViewController, animated: true)
+             }
+         }
+         
+         if currentSection == 3 {
+             if indexPath.row == 0 {
+                 let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                 let newViewController = storyBoard.instantiateViewController(withIdentifier: "HelpCenterVC")
+                 navigationController?.pushViewController(newViewController, animated: true)
+             }
+         }
+         if currentSection == 4{
+             if indexPath.row == 0 {
+                 let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                 let newViewController = storyBoard.instantiateViewController(withIdentifier: "MapSettingVC")
+                 navigationController?.pushViewController(newViewController, animated: true)
+             }
+             if indexPath.row == 1 {
+                 let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                 let newViewController = storyBoard.instantiateViewController(withIdentifier: "OpeninMapsVC")
+                 navigationController?.pushViewController(newViewController, animated: true)
+             }
+         }
+         
+     }
+     
+     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+         if indexPath.section == 0 {
+             return 100
+         }
+         else{
+             return 45
          }
      }
+     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+         if section == 0{
+             return 0
+         }
+         else{
+         return 40
+         }
+     }
+//   override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return 30
+//    }
+     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+         
+        let blurEffect = UIBlurEffect(style: .systemChromeMaterialLight)
+         let blurEffectView = UIVisualEffectView(effect: blurEffect)
+         
+         let headerView: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+         headerView.textLabel!.textColor = UIColor.darkGray
+         headerView.textLabel!.font = UIFont(name: "Roboto-Bold", size: 15)
+         //headerView.tintColor = .groupTableViewBackground
+         headerView.backgroundView = blurEffectView
+         //headerView.backgroundColor = UIColor(hex: "#F5F7F6")
+         
+         // For Header Text Color
+         let header = view as! UITableViewHeaderFooterView
+         header.textLabel?.textColor = .black
+         
+     }
+     
+     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+          super.traitCollectionDidChange(previousTraitCollection)
+
+          if #available(iOS 13.0, *) {
+              if self.traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+                  if traitCollection.userInterfaceStyle == .dark {
+                      print("dark mode")
+                  }
+                  else {
+                      //Light
+                  }
+              }
+          } else {
+              // Fallback on earlier versions
+          }
+      }
+    
+
+      override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+           return UIView()
+       }
 }
--- Extension/Extensions.swift
+++ Extension/Extensions.swift
@@ -8,43 +8,43 @@
 
 import Foundation
 import UIKit
-extension UIView {
-    
-    @IBInspectable var roundUpper : Bool{
-        get{
-            return true
-        }
-        set{
-            self.layer.cornerRadius = 15.0
-            self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-            self.clipsToBounds = true        }
-    }
-    
-    @IBInspectable var roundLower : Bool{
-        get{
-            return true
-        }
-        set{
-            self.layer.cornerRadius = 15.0
-            self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-            self.clipsToBounds = true
-            
-        }
-    }
-    
-    @IBInspectable var enableShadow : Bool{
-        get {
-            return true
-        }
-        set{
-//            let yourView = UIView()
-            self.layer.shadowColor = UIColor.black.cgColor
-            self.layer.shadowOpacity = 1
-            self.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
-            self.layer.shadowRadius = 20.0
-        }
-    }
-}
+//extension UIView {
+//
+//    @IBInspectable var roundUpper : Bool{
+//        get{
+//            return true
+//        }
+//        set{
+//            self.layer.cornerRadius = 15.0
+//            self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+//            self.clipsToBounds = true        }
+//    }
+//
+//    @IBInspectable var roundLower : Bool{
+//        get{
+//            return true
+//        }
+//        set{
+//            self.layer.cornerRadius = 15.0
+//            self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+//            self.clipsToBounds = true
+//
+//        }
+//    }
+//
+//    @IBInspectable var enableShadow : Bool{
+//        get {
+//            return true
+//        }
+//        set{
+////            let yourView = UIView()
+//            self.layer.shadowColor = UIColor.black.cgColor
+//            self.layer.shadowOpacity = 1
+//            self.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+//            self.layer.shadowRadius = 20.0
+//        }
+//    }
+//}
 extension UIColor {
     public convenience init?(hex: String) {
         let r, g, b, a: CGFloat
